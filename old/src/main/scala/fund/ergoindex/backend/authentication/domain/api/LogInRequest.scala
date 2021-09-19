25b364f12ca181d7f56af2881b2c77161232863e
Theodore Ilie <theoilie.ti@gmail.com>
2021-09-19 14:20:44 -0700

    Remove single-build SBT project

--- old/src/main/scala/fund/ergoindex/backend/authentication/domain/api/LogInRequest.scala
+++
@@ -1,4 +0,0 @@
-package fund.ergoindex.backend
-package authentication.domain.api
-
-final case class LogInRequest(email: String, password: String)
--- old/src/main/scala/fund/ergoindex/backend/authentication/domain/api/SignUpRequest.scala
+++
@@ -1,4 +0,0 @@
-package fund.ergoindex.backend
-package authentication.domain.api
-
-final case class SignUpRequest(email: String, password: String)
